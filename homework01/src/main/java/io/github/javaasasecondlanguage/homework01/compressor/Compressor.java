747e6cde8e8dd3f56ee607887bb8e2f09342ae36
rybalkinsd <yan.brikl@gmail.com>
2020-09-17 21:32:11 +0300

    compression homework - description

--- homework01/src/main/java/io/github/javaasasecondlanguage/homework01/compressor/Compressor.java
+++ homework01/src/main/java/io/github/javaasasecondlanguage/homework01/compressor/Compressor.java
@@ -1,28 +1,32 @@
 package io.github.javaasasecondlanguage.homework01.compressor;
 
-/**
- * Given an array of characters chars, compress it using the following algorithm:
- *
- * Begin with an empty string s.
- * For each group of consecutive repeating characters in chars:
- * If the group's length is 1, append the character to s.
- * Otherwise, append the character followed by the group's length.
- * Return a compressed string.
- * The compressed string s should not be returned separately, but instead be stored in the input
- * character array chars. Note that group lengths that are 10 or longer will be split into multiple characters in chars.
- *
- * After you are done modifying the input array, return the new length of the array.
- *
- *
- * Follow up:
- * Could you solve it using only O(1) extra space?
- *
- *
- */
 public class Compressor {
     /**
-     * @param str array of chars to compress
+     * Given an array of characters, compress it using the following algorithm:
+     *
+     * Begin with an empty string s.
+     * For each group of consecutive repeating characters in chars:
+     * If the group's length is 1, append the character to s.
+     * Otherwise, append the character followed by the group's length.
+     * Return a compressed string.
+     *
+     * Follow up:
+     * Could you solve it using only O(1) extra space?
+     *
+     * Examples:
+     * a -> a
+     * aa -> a2
+     * aaa -> a3
+     * aaabb -> a3b2
+     * "" -> ""
+     * null -> Illegal argument
+     * 234 sdf -> Illegal argument
+     *
+     * @param str nullable array of chars to compress
+     *            str may contain illegal characters
      * @return a compressed array
+     * @throws IllegalArgumentException if str is null
+     * @throws IllegalArgumentException if any char is not in range 'a'..'z'
      */
     public char[] compress(char[] str) {
         throw new RuntimeException("Not implemented");
