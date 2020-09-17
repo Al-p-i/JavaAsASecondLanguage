35483a98284554f9a8059a414f9cba13f9345235
rybalkinsd <yan.brikl@gmail.com>
2020-09-17 20:20:51 +0300

    compression homework

---
+++ homework01/src/main/java/io/github/javaasasecondlanguage/homework01/compressor/Compressor.java
@@ -0,0 +1,30 @@
+package io.github.javaasasecondlanguage.homework01.compressor;
+
+/**
+ * Given an array of characters chars, compress it using the following algorithm:
+ *
+ * Begin with an empty string s.
+ * For each group of consecutive repeating characters in chars:
+ * If the group's length is 1, append the character to s.
+ * Otherwise, append the character followed by the group's length.
+ * Return a compressed string.
+ * The compressed string s should not be returned separately, but instead be stored in the input
+ * character array chars. Note that group lengths that are 10 or longer will be split into multiple characters in chars.
+ *
+ * After you are done modifying the input array, return the new length of the array.
+ *
+ *
+ * Follow up:
+ * Could you solve it using only O(1) extra space?
+ *
+ *
+ */
+public class Compressor {
+    /**
+     * @param str array of chars to compress
+     * @return a compressed array
+     */
+    public char[] compress(char[] str) {
+        throw new RuntimeException("Not implemented");
+    }
+}
---
+++ homework01/src/test/java/io/github/javaasasecondlanguage/homework01/compressor/CompressorTest.java
@@ -0,0 +1,91 @@
+package io.github.javaasasecondlanguage.homework01.compressor;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@Disabled
+class CompressorTest {
+    Compressor compressor;
+
+    @BeforeEach
+    void setup() {
+        compressor = new Compressor();
+    }
+
+    @Test
+    void nullIsIllegalArgument() {
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> compressor.compress(null)
+        );
+    }
+
+    @Test
+    void stringWithIllegalCharacters() {
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> compressor.compress("Што такое?!!1адинадин".toCharArray())
+        );
+    }
+
+    @Test
+    void noCompressionForSingleChar() {
+        var subject = "a".toCharArray();
+        assertArrayEquals(subject, compressor.compress(subject));
+    }
+
+    @Test
+    void noCompressionForSimpleString() {
+        var subject = "abc".toCharArray();
+        assertArrayEquals(subject, compressor.compress(subject));
+    }
+
+    @Test
+    void sample1() {
+        var subject = "aabbccc".toCharArray();
+        assertArrayEquals("a2b2c3".toCharArray(), compressor.compress(subject));
+    }
+
+    @Test
+    void sample2() {
+        var subject = "mcobtwwwwaokzngssaox".toCharArray();
+        assertArrayEquals("mcobtw4aokzngs2aox".toCharArray(), compressor.compress(subject));
+    }
+
+    @Test
+    void maxTest500() {
+
+        var subject = (
+                "ubejrewmdnujlnrsqfrtrwnscangpjkbocajvsqdevtxifpbdddsxlrggcqhyevuxhrwxfqvjkhwmqoydt"
+                + "yuocfvljxiiwsdhuxhryuwnjvckjjowffdusiourttfiiocudlukbyqfnnavbetjtfmlzbcppdjbjhts"
+                + "pcaeotbnucazkazzysaxlpsrkpbzxwygksyozmidrpvcdjbvpjkkcgcobhsojawgeozwohwzeglpkveh"
+                + "wehepadbzlueayltfdcrayscrcokggdglanvcustfolobekqteqpmabghvkedjaupghhiikdjdizarky"
+                + "ndxzlxcmuivxlayajsclfxyzumczqfzsvysquenftinkburtsassumpkghajevjgiqzebrgnhtggfwcf"
+                + "qwmokceesaynxetpmaqodzwiulnhnevtlappgpbwxpmyxpnhwmjmzzzpjjzijilltyrnwqosfrqzaytr"
+                + "lbgzzoxafianxvwcbl"
+        ).toCharArray();
+
+        var result = (
+                "ubejrewmdnujlnrsqfrtrwnscangpjkbocajvsqdevtxifpbd3sxlrg2cqhyevuxhrwxfqvjkhwmqoydty"
+                + "uocfvljxi2wsdhuxhryuwnjvckj2owf2dusiourt2fi2ocudlukbyqfn2avbetjtfmlzbcp2djbjhtsp"
+                + "caeotbnucazkaz2ysaxlpsrkpbzxwygksyozmidrpvcdjbvpjk2cgcobhsojawgeozwohwzeglpkvehw"
+                + "ehepadbzlueayltfdcrayscrcokg2dglanvcustfolobekqteqpmabghvkedjaupgh2i2kdjdizarkyn"
+                + "dxzlxcmuivxlayajsclfxyzumczqfzsvysquenftinkburtsas2umpkghajevjgiqzebrgnhtg2fwcfq"
+                + "wmokce2saynxetpmaqodzwiulnhnevtlap2gpbwxpmyxpnhwmjmz3pj2zijil2tyrnwqosfrqzaytrlb"
+                + "gz2oxafianxvwcbl"
+        ).toCharArray();
+
+        assertArrayEquals(result, compressor.compress(subject));
+    }
+
+    @Test
+    void emptyArrayIsAlreadyCompressed() {
+        var emptyCharArray = new char[]{};
+        var result = compressor.compress(emptyCharArray);
+        assertArrayEquals(emptyCharArray, result);
+    }
+}
