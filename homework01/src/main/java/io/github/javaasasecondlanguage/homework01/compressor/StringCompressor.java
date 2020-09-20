72e996c84bff1cbed29fefc81278b80fe9af8884
Fedor Butikov <f.butikov@corp.mail.ru>
2020-09-21 00:12:13 +0300

    Homework01 - Fedor Butikov

--- homework01/src/main/java/io/github/javaasasecondlanguage/homework01/compressor/StringCompressor.java
+++ homework01/src/main/java/io/github/javaasasecondlanguage/homework01/compressor/StringCompressor.java
@@ -1,18 +1,20 @@
 package io.github.javaasasecondlanguage.homework01.compressor;
 
+import java.util.Arrays;
+
 public class StringCompressor {
     /**
      * Given an array of characters, compress it using the following algorithm:
-     *
+     * <p>
      * Begin with an empty string s.
      * For each group of consecutive repeating characters in chars:
      * If the group's length is 1, append the character to s.
      * Otherwise, append the character followed by the group's length.
      * Return a compressed string.
-     *
+     * <p>
      * Follow up:
      * Could you solve it using only O(1) extra space?
-     *
+     * <p>
      * Examples:
      * a -> a
      * aa -> a2
@@ -29,6 +31,58 @@ public class StringCompressor {
      * @throws IllegalArgumentException if any char is not in range 'a'..'z'
      */
     public char[] compress(char[] str) {
-        throw new RuntimeException("Not implemented");
+        if (str == null) {
+            throw new IllegalArgumentException("str is null");
+        }
+        if (str.length == 0) {
+            return str;
+        }
+        if (str[0] < 'a' || str[0] > 'z') {
+            throw new IllegalArgumentException("Got " + str[0] + " on position " + 0 +
+                    "only lower-case latin chars accepted.");
+        }
+        char prev = str[0];
+        int len = 1;
+        int newPos = 0;
+        int lenlen = 1;
+        int prevLen = 1;
+        for (int i = 1; i < str.length; ++i) {
+            if (str[i] == prev) {
+                ++len;
+                if (len / 10 >= prevLen) {
+                    ++lenlen;
+                    prevLen *= 10;
+                }
+            } else {
+                if (str[i] < 'a' || str[i] > 'z') {
+                    throw new IllegalArgumentException("Got " + str[i] + " on position " + i +
+                            "only lower-case latin chars accepted.");
+                }
+                str[newPos] = prev;
+                if (len != 1) {
+                    for (var j = lenlen; j > 0; --j) {
+                        str[newPos + j] = Integer.toString(len % 10).charAt(0);
+                        len /= 10;
+
+                    }
+                    newPos += lenlen;
+                }
+                ++newPos;
+                len = lenlen = prevLen = 1;
+                prev = str[i];
+
+            }
+        }
+        str[newPos] = prev;
+        if (len != 1) {
+            for (var j = lenlen; j > 0; --j) {
+                str[newPos + j] = Integer.toString(len % 10).charAt(0);
+                len /= 10;
+
+            }
+            newPos += lenlen;
+        }
+        ++newPos;
+        return Arrays.copyOfRange(str, 0, newPos);
     }
 }
--- homework01/src/main/java/io/github/javaasasecondlanguage/homework01/mergeintervals/IntervalsMerger.java
+++ homework01/src/main/java/io/github/javaasasecondlanguage/homework01/mergeintervals/IntervalsMerger.java
@@ -1,5 +1,9 @@
 package io.github.javaasasecondlanguage.homework01.mergeintervals;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class IntervalsMerger {
     /**
      * Given array of intervals, merges overlapping intervals and sort by start in ascending order
@@ -16,6 +20,32 @@ public class IntervalsMerger {
      * @throws IllegalArgumentException if intervals is null
      */
     public int[][] merge(int[][] intervals) {
-        throw new RuntimeException("Not implemented");
+        if (intervals == null) {
+            throw new IllegalArgumentException("intervals is null");
+        }
+        if (intervals.length < 2) {
+            return intervals;
+        }
+        Arrays.sort(intervals, (t1, t2) -> {
+            int result = Integer.compare(t1[0], t2[0]);
+            if (result == 0) {
+                return Integer.compare(t1[1], t2[1]);
+            }
+            return result;
+        });
+        ArrayList<int[]> result = new ArrayList<>();
+        result.add(intervals[0]);
+        int current_res_interval = 0;
+        for (int i = 1; i < intervals.length; ++i) {
+            if (intervals[i][0] <= result.get(current_res_interval)[1]) {
+                if (intervals[i][1] > result.get(current_res_interval)[1]) {
+                    result.get(current_res_interval)[1] = intervals[i][1];
+                }
+            } else {
+                result.add(intervals[i]);
+            }
+        }
+        int[][] temp = new int[result.size()][2];
+        return result.toArray(temp);
     }
 }
