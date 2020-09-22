51beba930137fb52b71c6f91f36f6042535efba4
artex <artex@mail.ru>
2020-09-22 19:34:14 +0300

    Добавил решения для StringCompressor'a и IntervalsMerger'a

--- homework01/src/main/java/io/github/javaasasecondlanguage/homework01/compressor/StringCompressor.java
+++ homework01/src/main/java/io/github/javaasasecondlanguage/homework01/compressor/StringCompressor.java
@@ -1,18 +1,20 @@
 package io.github.javaasasecondlanguage.homework01.compressor;
 
+import java.util.Arrays;
+
 public class StringCompressor {
     /**
      * Given an array of characters, compress it using the following algorithm:
-     *
+     * <p>
      * Begin with an empty string s.
      * For each group of consecutive repeating characters in chars:
      * If the group's length is 1, append the character to s.
      * Otherwise, append the character followed by the group's length.
      * Return a compressed string.
-     * </p>
+     * <p>
      * Follow up:
      * Could you solve it using only O(1) extra space?
-     * </p>
+     * <p>
      * Examples:
      * a -> a
      * aa -> a2
@@ -29,6 +31,45 @@ public class StringCompressor {
      * @throws IllegalArgumentException if any char is not in range 'a'..'z'
      */
     public char[] compress(char[] str) {
-        throw new RuntimeException("Not implemented");
+        checkNullityThrowException(str);
+        if (str.length == 0) {
+            return new char[]{};
+        }
+        byte count = 1;
+        int writePosition = 1;
+        checkLegalCharThrowExceptionIfFailed(str, 0);
+        for (int i = 1; i < str.length; i++) {
+            checkLegalCharThrowExceptionIfFailed(str, i);
+            if (str[i] == str[i - 1]) {
+                count++;
+                if (i + 1 == str.length) {
+                    str[writePosition++] = (char) (count + '0');
+                }
+            } else if (count > 1) {
+                str[writePosition++] = (char) (count + '0');
+                str[writePosition++] = str[i];
+                count = 1;
+            } else {
+                str[writePosition++] = str[i];
+            }
+        }
+        return Arrays.copyOfRange(str, 0, writePosition);
+    }
+
+    private void checkNullityThrowException(char[] str) {
+        if (str == null) {
+            throw new IllegalArgumentException("Passed illegal array!");
+        }
+    }
+
+    private void checkLegalCharThrowExceptionIfFailed(char[] chr, int index) {
+        if (index < chr.length && !checkLegalChars(chr[index])) {
+            String errorMessage = String.format("Illegal character in array! [index = {0}]", index);
+            throw new IllegalArgumentException(errorMessage);
+        }
+    }
+
+    private boolean checkLegalChars(char str) {
+        return str >= 'a' && str <= 'z';
     }
 }
--- homework01/src/main/java/io/github/javaasasecondlanguage/homework01/mergeintervals/IntervalsMerger.java
+++ homework01/src/main/java/io/github/javaasasecondlanguage/homework01/mergeintervals/IntervalsMerger.java
@@ -1,8 +1,12 @@
 package io.github.javaasasecondlanguage.homework01.mergeintervals;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class IntervalsMerger {
     /**
-     * Given array of intervals, merge overlapping intervals and sort them by start in ascending order
+     * Given array of intervals, merge overlapping intervals and sort
+     * them by start in ascending order
      * Interval is defined as [start, end] where start < end
      * <p>
      * Examples:
@@ -16,6 +20,58 @@ public class IntervalsMerger {
      * @throws IllegalArgumentException if intervals is null
      */
     public int[][] merge(int[][] intervals) {
-        throw new RuntimeException("Not implemented");
+        checkArgumentThrowException(intervals);
+        sortIntervals(intervals);
+        int removedElements = mergeIntervals(intervals);
+        return createFilteredArray(intervals, removedElements);
     }
+
+    private void checkArgumentThrowException(int[][] intervals) {
+        if (intervals == null) {
+            throw new IllegalArgumentException("Passed null intervals!");
+        }
+    }
+
+    private void sortIntervals(int[][] intervals) {
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0] > o2[0]) {
+                    return 1;
+                } else if (o1[0] < o2[0]) {
+                    return -1;
+                } else if (o1[1] > o2[1]) {
+                    return 1;
+                } else if (o1[1] < o2[1]) {
+                    return -1;
+                }
+                return 0;
+            }
+        });
+    }
+
+    private int mergeIntervals(int[][] intervals) {
+        int removedElements = 0;
+        for (int i = 0; i < intervals.length - 1; i++) {
+            if (intervals[i][0] <= intervals[i + 1][0] && intervals[i][1] >= intervals[i + 1][0]) {
+                intervals[i + 1][0] = Math.min(intervals[i][0], intervals[i + 1][0]);
+                intervals[i + 1][1] = Math.max(intervals[i][1], intervals[i + 1][1]);
+                intervals[i] = null;
+                removedElements++;
+            }
+        }
+        return removedElements;
+    }
+
+    private int[][] createFilteredArray(int[][] intervals, int removedElements) {
+        int[][] newIntervals = new int[intervals.length - removedElements][];
+        for (int i = 0, j = 0; i < intervals.length; ++i) {
+            if (intervals[i] != null) {
+                newIntervals[j++] = intervals[i];
+            }
+        }
+        return newIntervals;
+    }
+
+
 }
